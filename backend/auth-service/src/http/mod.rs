4eed58b5ac46d544058b6bb0f346e2ff1b1630e3
Tisca Catalin <admin@tiscacatalin.com>
2025-09-05 21:36:26 +0300

    properly creating the app http thang

--- backend/auth-service/src/http/mod.rs
+++ backend/auth-service/src/http/mod.rs
@@ -1,6 +1,18 @@
 use crate::db;
 use std::sync::Arc;
+use axum::{routing::get, Router};
 use tokio::net::TcpListener;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db: Arc<db::Database>,
+}
+
+pub fn app(db: Arc<db::Database>) -> Router {
+    Router::new()
+        .route("/health", get(|| async { "OK" }))
+        .with_state(AppState { db })
+}
 
 pub async fn run(db: Arc<db::Database>) -> Result<(), Box<dyn std::error::Error>> {
     let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
@@ -8,8 +20,7 @@ pub async fn run(db: Arc<db::Database>) -> Result<(), Box<dyn std::error::Error>
         .await
         .unwrap();
     tracing::info!("auth service is running on port {}", port);
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        tracing::debug!("new connection: {}", socket.peer_addr().unwrap());
-    }
+    axum::serve(listener, app(db).into_make_service())
+        .await?;
+    Ok(())
 }

872357b8c5a10e7018ba7ef289cf568ee5cb35c6
Tisca Catalin <admin@tiscacatalin.com>
2025-09-06 12:12:08 +0300

    serializable domain models on common

--- backend/common/src/domain/notes.rs
+++ backend/common/src/domain/notes.rs
@@ -1,5 +1,7 @@
 use time::OffsetDateTime;
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize)]
 pub struct Note {
     pub id: i32,
     pub user_id: i32,
--- backend/common/src/domain/user.rs
+++ backend/common/src/domain/user.rs
@@ -1,5 +1,7 @@
 use time::OffsetDateTime;
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
     pub username: String,
